c3ab8b88a81f168a7d0df264fb82a68ac5848f7f
AsharRehman450 <asharrehmanali@gmail.com>
2024-12-21 01:21:13 +0500

    add files and folder

---
+++ src/components/Card.jsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+  return (
+    <div className="section-padding">
+      <h1 className="heading1" id='smrt'>SmartPhone</h1>
+      
+      <div className="card-grid">
+        
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C63</h3>
+            <p className="card-description">3 min Charge, 60 min t20 Match</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/1717742184050e38a613e33f146b49dfa47720aea45cf.png.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme Note 50</h3>
+            <p className="card-description">Long-lasting Values Beast</p>
+            <p className="card-price">89,999</p> 
+          </div>
+          <div className="card-image">
+            <img src="/public/170850139998146c2183ef4824e00ab6ed688ebc55037.png.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C67</h3>
+            <p className="card-description">108MP 3x In-sensor Zoom Camera, Snapdragon 685</p>
+            <p className="card-price">149,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/1704419834105be51d94716d4469084d9ba9b1a904eb6.png.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme 13 + 5G</h3>
+            <p className="card-description">Speed to Victory</p>
+            <p className="card-price">26,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img4.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme GT 6</h3>
+            <p className="card-description">Power meets AI</p>
+            <p className="card-price">32,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img5.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme Note 60</h3>
+            <p className="card-description">Champion Quality</p>
+            <p className="card-price">49,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img6.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C61</h3>
+            <p className="card-description">Hard to break</p>
+            <p className="card-price">59,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img7.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C65</h3>
+            <p className="card-description">Unbreakable Champion</p>
+            <p className="card-price">74,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img8.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme 12</h3>
+            <p className="card-description">Power in Every Shot</p>
+            <p className="card-price">39,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img9.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme 12 + 5G</h3>
+            <p className="card-description">Power in Every Shot</p>
+            <p className="card-price">24,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img10.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C75</h3>
+            <p className="card-description">Dhulai Horai Hai</p>
+            <p className="card-price">39,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img11.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card-content">
+            <h2 className="card-new">NEW</h2>
+            <h3 className="card-title">realme C53</h3>
+            <p className="card-description">Champion Gold, 33W Champion Charge</p>
+            <p className="card-price">39,999</p>
+          </div>
+          <div className="card-image">
+            <img src="/public/img12.webp" alt="realme C75" className="card-img" />
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Card;
